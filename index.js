efa77805ca84c65f62a739f67b62ccecedf2f7ec
BarisKarapelit <baris_karapelit_298@hotmail.com>
2022-03-15 21:49:39 +0300

    Arrow Function Ders Notlari

---
+++ index.js
@@ -0,0 +1,12 @@
+
+//console.log("Merhaba Arkadaşlar") 
+
+// const demo =function(x,y)
+// {
+//     return x+y
+// }
+
+// console.log(demo(5,7))
+const demoarray =(x,y)=>x+y 
+
+console.log(demoarray(7.5,89))
